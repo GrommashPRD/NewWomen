7eb772b75623b1e91bd38ed20be7c1ee7516e5d8
GrommashPRD <epavel425@gmail.com>
2024-06-06 15:44:52 +0300

    Added

---
+++ coolsite/women/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from .models import *
+
+
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'get_html_photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
+    prepopulated_fields = {"slug": ("title",)}
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'get_html_photo', 'is_published')
+    readonly_fields = ('time_create', 'time_update', 'get_html_photo')
+
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width=50>")
+
+    get_html_photo.short_description = 'Миниатюра'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
